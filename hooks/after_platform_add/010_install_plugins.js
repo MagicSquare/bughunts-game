acb9c65efdeceb72b8733f48681b02887ec0476e
Jérémy Bouny <jbouny@gmail.com>
2015-07-09 16:56:41 +0200

    Initial commit of the 'La chasse aux bugs' web game.

---
+++ hooks/after_platform_add/010_install_plugins.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+ 
+//this hook installs all your plugins
+ 
+// add your plugins to this list--either 
+// the identifier, the filesystem location 
+// or the URL
+var pluginlist = [
+    "com.ionic.keyboard",
+    "cordova-plugin-console",
+    "cordova-plugin-crosswalk-webview",
+    "cordova-plugin-device",
+    "cordova-plugin-splashscreen",
+    "cordova-plugin-whitelist"
+];
+ 
+// no need to configure below
+ 
+var fs = require('fs');
+var path = require('path');
+var sys = require('sys')
+var exec = require('child_process').exec;
+ 
+function puts(error, stdout, stderr) {
+    sys.puts(stdout)
+}
+ 
+pluginlist.forEach(function(plug) {
+    exec("cordova plugin add " + plug, puts);
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,92 @@
+angular.module('starter.controllers', [])
+
+.controller('GameCtrl', function($scope, $http, Settings) {
+
+	var tabletSize = 4;
+
+	$scope.tablet = [];
+	$scope.tommettes = ['left', 'forward', 'right', 'back', 'remove'];
+	$scope.tommettesCmd = {'left': 'LE', 'forward': 'FO', 'right': 'RI', 'back': 'BA'};
+	$scope.gameImage = 'https://placeholdit.imgix.net/~text?txtsize=23&txt=Chargement...&w=300&h=300';
+	$scope.getTommetteUrl = function getTommetteUrl(tommette) {
+		return 'img/icons/' + tommette + '.png';
+	}
+
+	$scope.clear = function clear() {
+		$scope.tablet = [];
+	};
+
+	$scope.run = function run() {
+		var commands = []
+		for(var i = 0; i < $scope.tablet.length; ++i) {
+			commands.push($scope.tommettesCmd[$scope.tablet[i].icon]);
+		}
+		var command = commands.join(' ');
+		var url = Settings.host + '/command/' + command + '?callback=JSON_CALLBACK';
+		console.log(url);
+		$http.jsonp(url)
+			.success(function(data) {
+				$scope.gameImage = data.image;
+			});
+	}
+
+	$scope.handleTommette = function handleTommette(icon) {
+		if('remove' === icon) {
+			$scope.tablet.pop();
+		}
+		else {
+			var id = 0, length = $scope.tablet.length;
+			if(length > 0) {
+				var prevTommette = $scope.tablet[length-1];
+				id = prevTommette.gridster.row * tabletSize + prevTommette.gridster.col + 1;
+			}
+			if(id < tabletSize * tabletSize) {
+				$scope.tablet.push({
+					gridster: {
+						sizeX: 1,
+						sizeY: 1,
+						row: Math.floor(id / tabletSize),
+						col: id % tabletSize
+					}, 
+					icon: icon
+				});
+			}
+		}
+	};
+
+	$scope.handleTommette('forward');
+	$scope.run();
+
+	function orderTommettes() {
+		console.log('order');
+		$scope.tablet.sort(function(a, b) {
+			if(a.gridster.row != b.gridster.row)
+				return a.gridster.row - b.gridster.row;
+			return a.gridster.col - b.gridster.col;
+		});
+	}
+
+	$scope.gridsterOpts = {
+	    columns: tabletSize,
+	    minColumns: tabletSize,
+	    minRows: tabletSize,
+	    maxRows: tabletSize,
+	    pushing: true,
+	    floating: false,
+	    swapping: true,
+	    margins: [5, 5],
+	    outerMargin: true,
+	    mobileModeEnabled: false,
+	    resizable: {
+	       enabled: false,
+	    },
+	    draggable: {
+	       enabled: true,
+	       stop: function(event, $element, widget) { orderTommettes(); }
+	    }
+	};
+})
+
+.controller('SettingsCtrl', function($scope, Settings) {
+  $scope.settings = Settings;
+});
