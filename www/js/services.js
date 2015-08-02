8bfe0bb7d3ccec27e5a87513dc09609e6897faf7
hiks <kitnhiks@gmail.com>
2015-08-03 00:53:06 +0200

    new server for the services

--- www/js/services.js
+++ www/js/services.js
@@ -2,7 +2,7 @@ angular.module('starter.services', [])
 
 .service('Settings', function() {
   
-  this.host = 'http://lachasseauxbugs.fr/service';
+  this.host = 'http://service.lachasseauxbugs.fr:8111';
   //this.host = 'http://127.0.0.1:8111';
 
 })
